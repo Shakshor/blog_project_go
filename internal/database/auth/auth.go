26e66f2daa0bbfede729ed6f3608098bdcedc52b
shakshor <shakshor584@gmail.com>
2025-06-26 12:42:56 +0600

    some changes in get api key header

--- internal/database/auth/auth.go
+++ internal/database/auth/auth.go
@@ -13,7 +13,7 @@ import (
 func GetAPIKey(headers http.Header) (string, error) {
 	val := headers.Get("Authorization")
 	if val == "" {
-		return "", errors.New("no authentication info found")
+		return "", errors.New("no authentication information found")
 	}
 
 	vals := strings.Split(val, " ")
